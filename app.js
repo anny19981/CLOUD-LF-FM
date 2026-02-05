3c19db8aa28641075c79b4b0b26c73a5dd8db160
anny19981 <aholzer38@gmail.com>
2026-02-05 23:29:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -83,14 +83,14 @@ const songs = [
     artist: "Rolexander",
     file: "meine-jungs.mp3",
     cover: "cover3.jpg"
-  }
+  },
   {
     id: 12,
     title: "Megabonk",
     artist: "Rolexander",
     file: "megabonk.mp3",
     cover: "cover3.jpg"
-  }
+  },
 {
     id: 13,
     title: "Ich und meine Jungs",
